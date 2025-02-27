980acb98f292e3724174505a8a8d7d433e417f49
Tomas Bindokas <31992244+tbindokas13@users.noreply.github.com>
2025-02-27 11:21:57 -0600

    Add: Solution to odin-javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+// Sum of the first n natural number formula is n(n+1)/2
+const sumAll = function (startNumber, endNumber) {
+  if (!(isPositiveInteger(startNumber) && isPositiveInteger(endNumber)))
+    return "ERROR";
+  return (Math.abs(endNumber-startNumber)+1) * (startNumber + endNumber) / 2;
 };
+
+function isPositiveInteger(input) {
+  return Number.isInteger(input) && input > 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;

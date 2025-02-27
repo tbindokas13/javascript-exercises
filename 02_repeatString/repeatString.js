7a0d6e0688ed0a75e1ed0de59d87b4af6cf68ed8
Tomas Bindokas <31992244+tbindokas13@users.noreply.github.com>
2025-02-27 10:50:37 -0600

    Add: Solution to odin-javascript exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(word,repeatTimes) {
+    if(repeatTimes<0) return "ERROR";
+    let output=""
+    for(let i=0;i<repeatTimes;i++) output +=word;
+    return output;
 };
 
 // Do not edit below this line
